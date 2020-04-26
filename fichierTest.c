dba7adcbfdeffdb28e06be4ad5eadee86f716927
mramilison <mickael.ramilison@etu.upmc.fr>
2020-04-26 15:39:49 +0200

    initial commit

---
+++ fichierTest.c
@@ -0,0 +1,13 @@
+int start = 100;
+int end = 0;
+
+void main () {
+  int i , s = start , e = end ;
+  int sum = 0;
+
+  for ( i = s ; i < e ; i = i + 1) {
+    sum = sum + i ;
+  }
+  print ( " sum : " , sum , " \ n " ) ;
+}
+//Ceci est un commentaire
