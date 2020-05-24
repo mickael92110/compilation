3b8796184905aaefb6875565a356a42f97ee6209
mramilison <mickael.ramilison@etu.upmc.fr>
2020-05-24 16:47:58 +0200

    maj passe1.c

--- passe1.c
+++ passe1.c
@@ -1,39 +1,40 @@
 #include "passe1.h"
 
 void passe1(node_t root){
-  printf("J'existe\n");
   parcours_arbre(root);
 }
 
 
+
+// Cette fonction parcours l'arbre de maniere recursive
 void parcours_arbre(node_t node){
+  // On stock dans la variable temp, node->opr pour pouvoir retablir l'adresse original apres
   node_t * temp;
 
   if(node == NULL){
-    printf("NULL\n");
+    printf("feuille NULL\n");
     return;
   }
 
+  // Si on atteint l'extremite d'une branche on remonte
   if(node->opr == NULL){
-    printf("feuille :%s   ligne : %d\n",node_nature2string(node->nature), node->lineno);
+    printf("feuille : %s   ligne : %d\n",node_nature2string(node->nature), node->lineno);
     return;
   }
 
-  if(node->opr[0] != NULL){
-    //printf("here\n");
-    temp = node->opr;
-  }
+  // Stockage dans temp
+  temp = node->opr;
 
   printf("%s   ligne : %d\n",node_nature2string(node->nature), node->lineno);
-  //node_t temp = node->opr[0];
 
   parcours_arbre(node->opr[0]);
+
+  // Selon le nombre d'operateur on va parcourir plus ou moins de branches
   for(int i = 1; i<node->nops; ++i){
     ++node->opr;
     parcours_arbre(node->opr[0]);
     }
 
-
+  // On retablie la valeur de node->opr
   node->opr = temp;
-
 }
